391ea6bb03e8f7ae72a28a19340c6ca27c1f1e99
刘兴 <liuxing87327@163.com>
2014-11-01 22:58:43 +0800

    无具体功能更新

--- src/main/java/com/dooioo/db/MongoDbClient.java
+++ src/main/java/com/dooioo/db/MongoDbClient.java
@@ -33,7 +33,7 @@ public class MongoDbClient {
 	private static MongoDbClient client;
 	
     private DB db;
-    private boolean isLocal = false;
+    private boolean isLocal = true;
     
     private MongoDbClient() throws Exception {
 
